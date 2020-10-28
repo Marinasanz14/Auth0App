1098f32b918cd248fc330191c39c20928646a13a
Marina Sanchez <masaro1411@gmail.com>
2020-10-28 00:20:12 -0600

    instala jdk de auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +19,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-f52lui7m.us.auth0.com',
+      clientId: 'J2ASfJwl9wNdH9bLUFTOU7CRJgeLH3gb'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
